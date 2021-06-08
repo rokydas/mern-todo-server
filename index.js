b1bb04147dcc45a059eace2e2a20288f1b00c4dd
rokydas <droky136@gmail.com>
2021-06-08 15:07:06 +0600

    session 2 done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const cors = require('cors');
 const ObjectID = require('mongodb').ObjectID;
+const admin = require('firebase-admin');
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,12 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+var serviceAccount = require("./todo-mern18-firebase-adminsdk-x5z5m-a276ccbc8c.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://todo-mern:todo-mern@cluster0.17hrv.mongodb.net/todo-mern?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -18,48 +25,60 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     const todosCollection = client.db("todo-mern").collection("todos");
 
-    app.get('/todos', (req, res) => {
-        todosCollection.find({}).toArray((err, documents) => {
-            res.send(documents);
-        })
+    app.get('/todos/:email', (req, res) => {
+        const email = req.params.email;
+        const bearer = req.headers.bearer;
+        if (bearer && bearer.startsWith('Bearer ')) {
+            const tokenFromUi = bearer.split(' ')[1];
+            admin
+                .auth()
+                .verifyIdToken(tokenFromUi)
+                .then((decodedToken) => {
+                    const emailFromFirebase = decodedToken.email;
+                    if (email == emailFromFirebase) {
+                        todosCollection.find({ email: email }).toArray((err, documents) => {
+                            res.send(documents);
+                        })
+                    }
+                    else {
+                        res.send({msg: 'you are not authorized'});
+                    }
+                })
+                .catch((error) => {
+                    // Handle error
+                });
+        }
+        else {
+            res.send({msg: 'you are not authorized'});
+        }
+
     });
 
     app.post('/addTodo', (req, res) => {
         const todo = req.body;
         todosCollection.insertOne(todo)
-        .then(result => {
-            res.send({count: result.insertedCount})
-        }) 
+            .then(result => {
+                res.send({ count: result.insertedCount })
+            })
     });
 
     app.delete('/delete/:id', (req, res) => {
         const id = req.params.id;
-        todosCollection.deleteOne({_id: ObjectID(id)})
-        .then(result => {
-            res.send({count: result.deletedCount});
-        })
+        todosCollection.deleteOne({ _id: ObjectID(id) })
+            .then(result => {
+                res.send({ count: result.deletedCount });
+            })
     });
-
-    // app.patch('/update/:id', (req, res) => {
-    //     const id = req.params.id;
-    //     const todo = req.body;
-    //     todosCollection.updateOne({_id: ObjectID(id)}, {
-    //         $set: {title: todo.title, description: todo.description}
-    //     })
-    //     .then(result => {
-    //         res.send({count: result.modifiedCount});
-    //     })
-    // })
 
     app.patch('/update/:id', (req, res) => {
         const id = req.params.id;
         const todo = req.body;
-        todosCollection.updateMany({title: 'title1'}, {
-            $set: {title: todo.title, description: todo.description}
+        todosCollection.updateOne({ _id: ObjectID(id) }, {
+            $set: { title: todo.title, description: todo.description, img: todo.img }
         })
-        .then(result => {
-            res.send({count: result.modifiedCount});
-        })
+            .then(result => {
+                res.send({ count: result.modifiedCount });
+            })
     })
 
 });
